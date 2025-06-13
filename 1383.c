5e535c3e8366955d1e62f6dc6a5f5d181e09eb51
Luiz Tiago <luiztiagovaldivino27@gmail.com>
2025-06-13 13:29:08 -0300

    Update 1383.c

--- 1383.c
+++ 1383.c
@@ -1,51 +1,64 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool sudoku(int M[9][9]){
-
-    for(int i = 0; i < 9; i++){
-        int c[10];
-        for(int j = 0; j < 0; j++){
+bool sudoku(int M[9][9]) {
+    for(int i = 0; i < 9; i++) {
+        int c[10] = {0};
+        for(int j = 0; j < 9; j++) {
             int num = M[i][j];
-            if(num < 1 || num > 9 || c[num] != 0){
-            return false;
+            if(num < 1 || num > 9 || c[num] != 0) {
+                return false;
             }
             c[num]++;
         }
-
     }
-        for(int j = 0; j < 9; i++){
-        int c[10];
-        for(int i = 0; i < 0; j++){
+
+    for(int j = 0; j < 9; j++) {
+        int c[10] = {0};
+        for(int i = 0; i < 9; i++) {
             int num = M[i][j];
-            if(num < 1 || num > 9 || c[num] != 0){
-            return false;
+            if(num < 1 || num > 9 || c[num] != 0) {
+                return false;
             }
             c[num]++;
         }
+    }
 
+    for(int linhassudoku = 0; linhassudoku < 9; linhassudoku += 3) {
+        for(int colusudoku = 0; colusudoku < 9; colusudoku += 3) {
+            int c[10] = {0};
+            for(int i = linhassudoku; i < linhassudoku + 3; i++) {
+                for(int j = colusudoku; j < colusudoku + 3; j++) {
+                    int num = M[i][j];
+                    if(num < 1 || num > 9 || c[num] != 0) {
+                        return false;
+                    }
+                    c[num]++;
+                }
+            }
+        }
     }
     return true;
 }
 
-int main(){
-
-
-
+int main() {
     int n;
-    scanf("%i", &n);
-    for(int k = 1; k < n; k++){
+    scanf("%d", &n);
+    for(int k = 1; k <= n; k++) {
         int M[9][9];
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; i++){
-                scanf("%i", &M[i][j]);
+
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                scanf("%d", &M[i][j]);
             }
         }
-        printf("Instância %i\n", k);
-        if(sudoku(M)) printf("SIM\n");
-        else printf("Não\n");
-
-
-
+        printf("Instancia %d\n", k);
+        if(sudoku(M)) {
+            printf("SIM\n");
+        } else {
+            printf("NAO\n");
+        }
+        printf("\n");  
     }
+    return 0;
 }
