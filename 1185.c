a97733c020ee4ba50229018cf8702e2c0b7253a1
Luiz Tiago <luiztiagovaldivino27@gmail.com>
2025-06-13 11:56:10 -0300

    Create 1185.c

---
+++ 1185.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    double M[12][12];
+    char C;
+    double Soma = 0.0;
+    int e = 0;
+
+    scanf(" %c", &C);
+
+    for(int i = 0; i < 12; i++) {
+        for(int j = 0; j < 12; j++) {
+            scanf("%lf", &M[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            if(i+j < 11) {
+            Soma += M[i][j];
+            e++;
+            }
+        }
+    }  
+    if(C == 'S') {
+        printf("%.1lf\n", Soma);
+    } else if(C == 'M') {
+        printf("%.1lf\n", Soma / e);
+    }
+    
+    return 0;
+}
