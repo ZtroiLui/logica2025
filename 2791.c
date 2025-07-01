13806af3668d6f1942b15833ed121e4eb21eba0e
Luiz Tiago <luiztiagovaldivino27@gmail.com>
2025-07-01 12:48:55 -0300

    Create 2791.c

---
+++ 2791.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int C1 = 0, C2 = 0, C3 = 0, C4 = 0;
+    scanf("%i", &C1);
+    scanf("%i", &C2);
+    scanf("%i", &C3);
+    scanf("%i", &C4);
+    if(C1 == 1){
+        printf("%i\n", 1);
+    }
+    if(C2 == 1){
+        printf("%i\n", 2);
+    }
+    if(C3 == 1){
+        printf("%i\n", 3);
+    }
+    if(C4 == 1){
+        printf("%i\n", 4);
+    }
+    return 0;
+}
