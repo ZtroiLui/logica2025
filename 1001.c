0e934fd5e4313ab51ef608b45fd2dbfc14f46c89
Luiz Tiago <luiztiagovaldivino27@gmail.com>
2025-07-04 13:39:17 -0300

    Update 1001.c

--- 1001.c
+++ 1001.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
  
 int main() {
- int A = 0, B = 0, X=0;
+  int A = 0, B = 0, X=0;
  
- scanf("%i" "%i", &A, &B);
+  scanf("%i" "%i", &A, &B);
  
- X = A + B;
+  X = A + B;
  
- printf("X = %i\n", X);
+  printf("X = %i\n", X);
  
  
-    return 0;
+  return 0;
 }
